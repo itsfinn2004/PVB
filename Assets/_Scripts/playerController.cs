5610aadf9b1978abc12b3c87eb6f18f8ce91a25f
Finn <fstreunding@gmail.com>
2025-05-15 11:10:10 +0200

    feature movement werkt met toetsenbord

---
+++ Assets/_Scripts/playerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace FistFury
+    //deze script is gemaakt door Finn Streunding
+{
+    public class playerController : MonoBehaviour
+    {
+        private Vector2 movement;
+        public Rigidbody2D rb;
+        public float jumpForce = 7f;
+        private bool isGrounded;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        public void onHorizontalMove(InputAction.CallbackContext context)
+        {
+            float input = context.ReadValue<float>();
+            movement = new Vector2(input, 0f);
+        }
+
+        public void onVerticalMove(InputAction.CallbackContext context)
+        {
+            Debug.Log("spring test");
+            if(context.performed && isGrounded)
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+                isGrounded = false;
+
+            }
+        }
+
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if(collision.gameObject.CompareTag("Ground"))
+            {
+                isGrounded = true;
+            }
+        }
+
+        private void Update()
+        {
+            transform.Translate(movement * Time.deltaTime * 5f);
+        }
+    }
+}
