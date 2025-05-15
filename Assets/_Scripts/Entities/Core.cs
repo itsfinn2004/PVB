6ad57b7f636d65e8293d26bb61cf4ed753c8d1de
NiekMSoftware <niek+git@meletstudios.com>
2025-05-15 10:14:53 +0200

    feat: linked entity test to core
    also made sure the game object uses this object and made Core abstract

--- Assets/_Scripts/Entities/Core.cs
+++ Assets/_Scripts/Entities/Core.cs
@@ -7,8 +7,9 @@ namespace FistFury.Entities
     /// <summary>
     /// Overall abstract class for all the entities, containing all the core data.
     /// </summary>
-    public class Core : MonoBehaviour
+    public abstract class Core : MonoBehaviour
     {
+        [field: Header("References")]
         [field: SerializeField] public Animator Animator { get; private set; }
         [field: SerializeField] public Rigidbody2D Body { get; private set; }
 
--- Assets/_Scripts/Entities/Tests/EntityTest.cs
+++ Assets/_Scripts/Entities/Tests/EntityTest.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace FistFury.Entities.Tests
 {
     public class EntityTest : Core
