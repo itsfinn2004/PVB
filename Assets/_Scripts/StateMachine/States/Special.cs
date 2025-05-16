eb06c3959089ad18f3d6d0a758608737b047752d
Finn <fstreunding@gmail.com>
2025-05-16 13:58:13 +0200

    feat/alle states klaar gemaakt om te gebruiken

---
+++ Assets/_Scripts/StateMachine/States/Special.cs
@@ -0,0 +1,20 @@
+// Made by Finn Streunding on 16/5/2025
+
+using UnityEngine;
+
+namespace FistFury.StateMachine.States
+{
+    public class Special : State
+    {
+        [SerializeField] private AnimationClip anim;
+
+        public override void Enter()
+        {
+            base.Enter();
+
+            // play animation
+            if (Animator && anim)
+                Animator.Play(anim.name);
+        }
+    }
+}
