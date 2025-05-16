a745626d7d224fc77cde97b7fb58a6300c32e6b0
NiekMSoftware <niek+git@meletstudios.com>
2025-05-16 09:59:59 +0200

    fix: ctv filter doesn't render on scene view
    also adjusted settings of the renderer

--- Assets/_Scripts/Rendering/CrtSettings.cs
+++ Assets/_Scripts/Rendering/CrtSettings.cs
@@ -8,22 +8,27 @@ namespace FistFury.Rendering
     [System.Serializable]
     public class CrtSettings
     {
-        public RenderPassEvent RenderPassEvent =
-            UnityEngine.Rendering.Universal.RenderPassEvent.AfterRenderingPostProcessing;
-        public Material CRTMaterial = null;
+        [Header("General Settings")]
+        public bool IsEnabled = true;
+        public RenderPassEvent RenderPassEvent = RenderPassEvent.AfterRenderingPostProcessing;
+        public Material CRTMaterial;
 
+        [Header("Distortion")]
         [Range(0f, 0.3f)] public float Curvature = 0.1f;
         [Range(0f, 1f)] public float PaniniDistance = 0.3f;
         [Range(0f, 1f)] public float PaniniCropToFit = 0.5f;
 
+        [Header("Scan lines")]
         [Range(0f, 1f)] public float ScanlineIntensity = 0.5f;
         [Range(1f, 2000f)] public float ScanlineCount = 900f;
         [Range(-10, 10)] public float ScanlineSpeed = 2.0f;
 
+        [Header("Color Adjustments")]
         [Range(0f, 5f)] public float RgbShift = 0.5f;
         [Range(0.5f, 1.5f)] public float Brightness = 1.2f;
         [Range(0.5f, 1.5f)] public float Contrast = 1.1f;
 
+        [Header("Effects")]
         [Range(0f, 0.1f)] public float Flicker;
         [Range(0f, 1f)] public float VignetteIntensity = 0.3f;
         [Range(0f, 0.5f)] public float NoiseIntensity = 0.05f;
